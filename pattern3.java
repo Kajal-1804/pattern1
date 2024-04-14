ab546e464682fdc390d977e1f0dc4f8117035da0
kajal-1804 <kajaltiwari0418@gmail.com>
2024-04-15 05:01:02 +0530

    first commit

---
+++ pattern3.java
@@ -0,0 +1,13 @@
+public class pattern3{
+	public static void main(String[] args){
+	int i,j,r=6;
+	for(i=0;i<r;i++)
+	{
+	for(j=0;j<=i;j++)
+	{
+	System.out.print("*");
+	}
+	System.out.println();
+}
+}
+}
